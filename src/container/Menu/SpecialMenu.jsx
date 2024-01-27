13ff9ebfd0390927365147acae6785f0e77f5929
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-01-26 21:01:22 -0500

    completed SpecialMenu

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -7,10 +7,10 @@ const SpecialMenu = () => (
   <div className="app__specialMenu flex__center section__padding" id="menu">
     <div className="app__specialMenu-title">
       <SubHeading title="Menu that fits your palatte" />
-      <h1 className="headtext_cormorant">Today's Special</h1>
+      <h1 className="headtext__cormorant">Today's Special</h1>
     </div>
     <div className="app__specialMenu-menu">
-      <div className="app__specialMenu-menu_wine flex_center">
+      <div className="app__specialMenu-menu_wine flex__center">
         <p className="app__specialMenu-menu_heading">Wine & Beer</p>
         <div className="app__specialMenu_menu_items">
           {data.wines.map((wine, index) => (
@@ -23,11 +23,12 @@ const SpecialMenu = () => (
           ))}
         </div>
       </div>
-      <div className="app__specialMenu_menu img">
-        <img src={images.menu} alt="menu_img" />
+      
+      <div className="app__specialMenu-menu_img">
+        <img src={images.menu} alt="menu__img" />
       </div>
 
-      <div className="app__specialMenu-menu_cocktails flex_center">
+      <div className="app__specialMenu-menu_cocktails flex__center">
         <p className="app__specialMenu-menu_heading">Cocktails</p>
         <div className="app__specialMenu_menu_items">
           {data.cocktails.map((cocktail, index) => (
